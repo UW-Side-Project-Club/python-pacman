f2536f7b096258a28e046b038776d34c85ce082f
Amirmehdi Sharifzad <amsharifzad@gmail.com>
2018-06-26 19:23:48 -0400

    solved issue #1

--- pacman.py
+++ pacman.py
@@ -1,6 +1,12 @@
 # import the libraries
 import sys, pygame
 
+def get_angle(cur_angle, des_angle):
+    ang = des_angle-cur_angle
+    if ang < 0:
+        ang += 360
+    return ang
+
 # initialize pygame
 pygame.init()
 
@@ -9,6 +15,7 @@ size = width, height = 1024, 720
 
 # initially pacman does not move
 speed = [0,0]
+angle = 0
 
 # windows color
 black = 0, 0, 0   
@@ -30,22 +37,42 @@ while 1:
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                moving = True
-                speed = [-8, 0]
+                if pacmanrect.left >= 0:
+                    moving = True
+                    speed = [-8, 0]
+                    pacman = pygame.transform.rotate(pacman, get_angle(angle,180))
+                    angle = 180
             if event.key == pygame.K_RIGHT:
-                moving = True
-                speed = [8, 0]
+                if pacmanrect.right <= width:
+                    moving = True
+                    speed = [8, 0]
+                    pacman = pygame.transform.rotate(pacman, get_angle(angle, 0))
+                    angle = 0
             if event.key == pygame.K_UP:
-                moving = True
-                speed = [0, -8]
+                if pacmanrect.top >= 0:
+                    moving = True
+                    speed = [0, -8]
+                    pacman = pygame.transform.rotate(pacman, get_angle(angle, 90))
+                    angle = 90
             if event.key == pygame.K_DOWN:
-                moving = True
-                speed = [0, 8]
+                if pacmanrect.bottom <= height:
+                    moving = True
+                    speed = [0, 8]
+                    pacman = pygame.transform.rotate(pacman, get_angle(angle, 270))
+                    angle = 270
     if moving:
         pacmanrect = pacmanrect.move(speed)
-    if pacmanrect.left < 0 or pacmanrect.right > width:
+    if pacmanrect.left < 0:  
+        pacmanrect.left = 0
         moving = False
-    if pacmanrect.top < 0 or pacmanrect.bottom > height:
+    if pacmanrect.right > width:
+        pacmanrect.right = width
+        moving = False
+    if pacmanrect.top < 0:
+        pacmanrect.top = 0
+        moving = False
+    if pacmanrect.bottom > height:
+        pacmanrect.bottom = height
         moving = False
 
     screen.fill(black)
