f8722e36d5b5b0b3e33937ed5b34785d27958348
Amirmehdi Sharifzad <amsharifzad@gmail.com>
2018-06-26 20:11:36 -0400

    pacman rotates

--- pacman.py
+++ pacman.py
@@ -11,7 +11,7 @@ def get_angle(cur_angle, des_angle):
 pygame.init()
 
 # set size of the window
-size = width, height = 1024, 720
+size = width, height = 720, 360
 
 # initially pacman does not move
 speed = [0,0]
@@ -23,12 +23,14 @@ black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 
 # pacman image object
-pacman = pygame.image.load('pacman.gif')
+pacman = pygame.image.load('pacman24.png')
 # rectangle around pacman
 pacmanrect = pacman.get_rect()
 
 # pacman not moving initially
 moving = False
+
+facing = 'right'
 
 # the game loop
 while 1:
@@ -38,28 +40,38 @@ while 1:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 if pacmanrect.left >= 0:
-                    moving = True
-                    speed = [-8, 0]
                     pacman = pygame.transform.rotate(pacman, get_angle(angle,180))
                     angle = 180
+                    if facing == 'right':
+                        pacman = pygame.transform.flip(pacman, False, True)
+                        facing = 'left'
+                    moving = True
+                    speed = [-8, 0]
+                    
             if event.key == pygame.K_RIGHT:
                 if pacmanrect.right <= width:
-                    moving = True
-                    speed = [8, 0]
                     pacman = pygame.transform.rotate(pacman, get_angle(angle, 0))
                     angle = 0
+                    if facing == 'left':
+                        pacman = pygame.transform.flip(pacman, False, True)
+                        facing = 'right'
+                    moving = True
+                    speed = [8, 0]
+                    
             if event.key == pygame.K_UP:
                 if pacmanrect.top >= 0:
-                    moving = True
-                    speed = [0, -8]
                     pacman = pygame.transform.rotate(pacman, get_angle(angle, 90))
                     angle = 90
+                    moving = True
+                    speed = [0, -8]
+                    
             if event.key == pygame.K_DOWN:
                 if pacmanrect.bottom <= height:
-                    moving = True
-                    speed = [0, 8]
                     pacman = pygame.transform.rotate(pacman, get_angle(angle, 270))
                     angle = 270
+                    moving = True
+                    speed = [0, 8]
+                    
     if moving:
         pacmanrect = pacmanrect.move(speed)
     if pacmanrect.left < 0:  
